687c6d51f8b868af6fbc26b00fad3e7f26fb98bb
Wasiur Rahman <osiur2@gmail.com>
2022-10-02 19:27:38 +0600

    Add Date

--- src/Time.js
+++ src/Time.js
@@ -2,17 +2,24 @@ import { useEffect, useState } from "react";
 
 function Time() {
     const [time, setTime] = useState();
+    const [date, setDate] = useState();
+    const [day, setDay] = useState();
    
     useEffect(() => {
        
         setInterval(() => {
             const today = new Date();
-            setTime(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+            setTime("" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
         }, 100);
+        setInterval(() => {
+            const date = new Date();
+            setDate(date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " ");
+        });
     }, []);
 
     return (
         <div className="ml-2">
+            {date}
             {time}
         </div>
     );
